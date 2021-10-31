5d903c0b9012506306b45455f41bbc93f78432da
b1tsec <74562513+b1tsec@users.noreply.github.com>
2021-10-31 09:46:04 +0100

    Update opensslbrute.sh
    A script to bruteforce password protected openssl files

--- opensslbrute.sh
+++ opensslbrute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for pass in $(cat /opt/SecLists/Passwords/darkweb2017-top10000.txt); do
-	openssl enc -d -aes-256-cbc -a -in .drupal.txt.enc -k $pass > /dev/null 2>&1
+for pass in $(cat <password list>); do
+	openssl enc -d -aes-256-cbc -a -in <encrypted file> -k $pass > /dev/null 2>&1
 
 	ret=$?
 	if [ $ret -ne 1 ]; then
